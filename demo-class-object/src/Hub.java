371c8c0e2d12ecdaefd4ae2e0cc894225f13fbe2
samuelcsng <samuelcsng@gmail.com>
2024-08-16 11:04:09 +0800

    20240816.1

--- demo-class-object/src/Hub.java
+++ demo-class-object/src/Hub.java
@@ -2,28 +2,53 @@ public class Hub {
   private String hubName;
   private int numOfTransceivers;
   private int numOfCustomersServed;
+  private int maxServicedPerMachine;
+
+  public int getMaxServicedPerMachine() {
+    return maxServicedPerMachine;
+  }
+
+  public void setMaxServicedPerMachine(int maxServicedPerMachine) {
+    this.maxServicedPerMachine = maxServicedPerMachine;
+  }
+
+  public Hub(String hubName, int numOfTransceivers, int numOfCustomersServed) {
+    this.hubName = hubName;
+    this.numOfTransceivers = numOfTransceivers;
+    this.numOfCustomersServed = numOfCustomersServed;
+  }
 
   public String getHubName() {
     return hubName;
   }
+
   public void setHubName(String hubName) {
     this.hubName = hubName;
   }
+
   public int getNumOfTransceivers() {
     return numOfTransceivers;
   }
+
   public void setNumOfTransceivers(int numOfTransceivers) {
     this.numOfTransceivers = numOfTransceivers;
   }
+
   public int getNumOfCustomersServed() {
     return numOfCustomersServed;
   }
+
   public void setNumOfCustomersServed(int numOfCustomersServed) {
     this.numOfCustomersServed = numOfCustomersServed;
   }
 
-  public double customersServedPerMachine(){
+  public double customersServedPerMachine() {
     return this.numOfCustomersServed / this.numOfTransceivers;
   }
-  
+
+  public double maxNumberOfService() {
+    return this.maxServicedPerMachine * this.numOfTransceivers;
+  }
+
+
 }
